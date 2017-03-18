a9a27044fca0bb00cc04d0f136b5d2a0f3b16d49
Lennart Oldenburg <hello@lennartoldenburg.de>
2017-03-19 00:01:13 +0100

    Corrected handler to add a new removal element. Also includes visual improvements.

--- handlers/pipeline.go
+++ handlers/pipeline.go
@@ -27,10 +27,10 @@ func Pipeline(dbConner DBConner) gin.HandlerFunc {
 	return func(c *gin.Context) {
 
 		var Removals []models.Removal
-		dbConner.GetDBConn().Order("\"created\" desc").Find(&Removals)
+		dbConner.GetDBConn().Order("\"created\" asc").Find(&Removals)
 
 		var Replacements []models.Replacement
-		dbConner.GetDBConn().Order("\"created\" desc").Find(&Replacements)
+		dbConner.GetDBConn().Order("\"created\" asc").Find(&Replacements)
 
 		c.HTML(http.StatusOK, "pipeline-list.html", gin.H{
 			"PageTitle":    "Protokollamt der Freitagsrunde - Analyse-Pipeline",
@@ -50,49 +50,17 @@ func PipelineRemovalsAdd(dbConner DBConner) gin.HandlerFunc {
 		var Payload NewRemovalPayload
 		var Removal models.Removal
 
-		// Get currently existing removal elements.
-		var Removals []models.Removal
-		dbConner.GetDBConn().Order("\"created\" desc").Find(&Removals)
-
-		// Get currently existing replacement elements.
-		var Replacements []models.Replacement
-		dbConner.GetDBConn().Order("\"created\" desc").Find(&Replacements)
-
 		// Parse removal form data into above defined payload.
 		err := c.BindWith(&Payload, binding.FormPost)
 		if err != nil {
-
-			c.HTML(http.StatusBadRequest, "pipeline-list.html", gin.H{
-				"PageTitle":    "Protokollamt der Freitagsrunde - Analyse-Pipeline",
-				"MainTitle":    "Analyse-Pipeline",
-				"FatalError":   "Verarbeitungsfehler. Bitte erneut versuchen.",
-				"Removals":     Removals,
-				"Replacements": Replacements,
-			})
+			c.Redirect(http.StatusFound, "/pipeline")
 			c.Abort()
 			return
 		}
 
-		// Construct list of errors, if present.
-		errs := make(map[string]string)
-
-		if Payload.StartTag == "" {
-			errs["Start-Zeichenkette"] = "Bitte folgendes Feld ausfüllen"
-		}
-
-		if Payload.EndTag == "" {
-			errs["End-Zeichenkette"] = "Bitte folgendes Feld ausfüllen"
-		}
-
-		if len(errs) > 0 {
-
-			c.HTML(http.StatusBadRequest, "pipeline-list.html", gin.H{
-				"PageTitle":    "Protokollamt der Freitagsrunde - Analyse-Pipeline",
-				"MainTitle":    "Analyse-Pipeline",
-				"Errors":       errs,
-				"Removals":     Removals,
-				"Replacements": Replacements,
-			})
+		// Do not allow empty removal elements.
+		if Payload.StartTag == "" || Payload.EndTag == "" {
+			c.Redirect(http.StatusFound, "/pipeline")
 			c.Abort()
 			return
 		}
@@ -106,15 +74,7 @@ func PipelineRemovalsAdd(dbConner DBConner) gin.HandlerFunc {
 		// Save removal element to database.
 		dbConner.GetDBConn().Create(&Removal)
 
-		// Get updated list of removal elements.
-		dbConner.GetDBConn().Order("\"created\" desc").Find(&Removals)
-
-		c.HTML(http.StatusOK, "pipeline-list.html", gin.H{
-			"PageTitle":    "Protokollamt der Freitagsrunde - Analyse-Pipeline",
-			"MainTitle":    "Analyse-Pipeline",
-			"Removals":     Removals,
-			"Replacements": Replacements,
-		})
+		c.Redirect(http.StatusFound, "/pipeline")
 	}
 }
 
